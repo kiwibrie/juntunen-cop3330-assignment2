04894da725beb5839bb42b70445d6004bb2bd9bf
kiwibrie <brianne0128@gmail.com>
2021-06-11 15:43:40 -0500

    Initial commit

---
+++ src/main/java/ex24/App.java
@@ -0,0 +1,15 @@
+package ex24;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Scanner;
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+    }
+}

38cac66f18e426b518659c5741494056dbb00f37
kiwibrie <brianne0128@gmail.com>
2021-06-11 16:21:25 -0500

    ex24 app and tests done

--- src/main/java/ex24/App.java
+++ src/main/java/ex24/App.java
@@ -1,15 +1,49 @@
 package ex24;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Brianne Juntunen
  */
+
+//Anagram Checker
+import java.util.Arrays;
 import java.util.Scanner;
 public class App {
     static Scanner in = new Scanner(System.in);
 
     public static void main( String[] args ) {
+        App myApp = new App();
 
-        System.out.println( "Hello World!" );
+        System.out.println("Enter two strings and I'll tell you if they are anagrams: ");
+        String input1 = myApp.inputString("Enter the first string");
+        String input2 = myApp.inputString("Enter the second string");
+
+        boolean anagram = myApp.isAnagram(input1, input2);
+
+        myApp.printOutput(anagram, input1, input2);
+    }
+
+    public String inputString(String prompt){
+        System.out.printf("%s: ", prompt);
+        return in.nextLine();
+    }
+
+    public boolean isAnagram(String input1, String input2){
+        if(input1.length() != input2.length()){
+            return false;
+        }
+        char[] string1 = input1.toCharArray();
+        char[] string2 = input2.toCharArray();
+        Arrays.sort(string1);
+        Arrays.sort(string2);
+        return Arrays.equals(string1, string2);
+    }
+
+    public void printOutput(boolean anagram, String input1, String input2){
+        if(anagram){
+            System.out.printf("\"%s\" and \"%s\" are anagrams.", input1, input2);
+        } else {
+            System.out.printf("\"%s\" and \"%s\" are not anagrams.", input1, input2);
+        }
     }
 }
--- src/test/java/ex24/AppTest.java
+++ src/test/java/ex24/AppTest.java
@@ -1,20 +1,61 @@
 package ex24;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    App myApp = new App();
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @DisplayName("AreAnagrams")
+    public void areAnagrams() {
+        String input1 = "note";
+        String input2 = "tone";
+
+        assertTrue(myApp.isAnagram(input1, input2));
+    }
+
+    @Test @DisplayName("notAnagrams")
+    public void notAnagrams(){
+        String input1 = "anagram";
+        String input2 = "abcdefg";
+        assertFalse(myApp.isAnagram(input1, input2));
+    }
+
+    @Test @DisplayName("notLength")
+    public void notLength(){
+        String input1 = "string";
+        String input2 = "stirngg";
+        assertFalse(myApp.isAnagram(input1, input2));
+    }
+
+    @Test @DisplayName("spaceTrue")
+    public void spacesTrue(){
+        String input1 = "strin g";
+        String input2 = "st irng";
+        assertTrue(myApp.isAnagram(input1, input2));
+    }
+
+    @Test @DisplayName("spaceFalse")
+    public void spacesFalse(){
+        String input1 = "strin g";
+        String input2 = "st ir ng";
+        assertFalse(myApp.isAnagram(input1, input2));
+    }
+
+    @Test @DisplayName("endSpaces")
+    public void spacesEnd(){
+        String input1 = "string    ";
+        String input2 = "s t ir n g";
+        assertTrue(myApp.isAnagram(input1, input2));
+    }
+
+    @Test @DisplayName("exact match")
+    public void exactMatch(){
+        String input1 = "string";
+        String input2 = "string";
+        assertTrue(myApp.isAnagram(input1, input2));
     }
 }
